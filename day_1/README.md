d2ddbd9bec4ea8b4d95172d24e34c0ae2cbb2158
Tyler Perdue <perduetyler1@gmail.com>
2020-12-01 16:33:13 -0500

    part 2 w/ reflection

---
+++ day_1/README.md
@@ -0,0 +1,22 @@
+## Day 1
+
+### Part 1
+
+Find two numbers in `src/expense_report.txt` that add up to 2020 and return their product. 
+
+### Part 2
+
+Find three numbers in `src/expense_report.txt` that add up to 2020 and return their product.
+
+
+### Reflection
+
+Rust is hard. The compiler is so complex. Sometimes helpful, offering suggestions. Somethings speaking an entirely different language. 
+
+Struggled with the differences between `String`, `&String`, and `&str`. 
+
+Error handling is more verbose than Golang. At least so far...
+
+Love the `Option` and `Result` flow but wish there was a better way to bind and pipeline. 
+
+Once it's compiled, this stuff works. And it's fast. 
--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -14,19 +14,35 @@ fn read_report(s: String) -> Result<HashMap<i32, bool>, Box<dyn Error>> {
 	for line in s.split('\n') {
 		let entry = line.parse::<i32>()?;
 		entries.insert(entry, true);
-	};
+	}
 
 	Ok(entries)
 }
 
-fn find_mistake(r: HashMap<i32, bool>) -> Option<(i32, i32)> {
-	for (k, _v) in &r {
+fn find_pair(r: &HashMap<i32, bool>) -> Option<(i32, i32)> {
+	for (k, _v) in r {
 		let p = 2020 - k;
 
 		if r.contains_key(&p) {
 			return Some((*k, p));
 		}
-	};
+	}
+
+	None
+}
+
+fn find_triple(r: &HashMap<i32, bool>) -> Option<(i32, i32, i32)> {
+	for (ki, _vi) in r {
+		let p = 2020 - ki;
+
+		for (kj, _vj) in r {
+			let m = p - kj;
+
+			if r.contains_key(&m) {
+				return Some((*kj, *ki, m))
+			}
+		}
+	}
 
 	None
 }
@@ -34,12 +50,16 @@ fn find_mistake(r: HashMap<i32, bool>) -> Option<(i32, i32)> {
 
 fn main() {
 	let report = get_expense_report_from_archives().and_then(|s: String| read_report(s)).unwrap();
-	let mistake = find_mistake(report);
 
-	if mistake.is_none() {
-		panic!("no mistakes? that can't be right...")
+	let pair = find_pair(&report);
+	let triple = find_triple(&report);
+
+	if pair.is_none() || triple.is_none()  {
+		panic!("no matches? that can't be right...")
 	}
 
-	let answer = mistake.unwrap().0 * mistake.unwrap().1;
-	println!("answer: {}", answer)
+	let part_1 = pair.unwrap();
+	let part_2 = triple.unwrap();
+
+	println!("part 1: {} part 2: {}", (part_1.0 * part_1.1), (part_2.0 * part_2.1 *part_2.2))
 }
